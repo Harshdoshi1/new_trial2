739c5d402d24710848a7322d08a59fb3860434e2
Shyama Vagadia <115221784+Shyamavagadiya@users.noreply.github.com>
2024-12-30 09:53:14 +0530

    UI

--- lib/pages/progress_screen.dart
+++ lib/pages/progress_screen.dart
@@ -2,46 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:new_trial2/controllers/order_controller.dart';
 
+import '../utils/color_utils.dart';
+
+
+
 class ProgressScreen extends StatelessWidget {
   final OrderController orderController = Get.put(OrderController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Order Progress'),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              hexStringToColor("E7C6A5"),
+              hexStringToColor("F4DBD8"),
+              hexStringToColor("F4DBD8"),
+              hexStringToColor("E7C6A5"),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Obx(() {
+          if (orderController.orders.isEmpty) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return ListView.builder(
+            itemCount: orderController.orders.length,
+            itemBuilder: (context, orderIndex) {
+              final order = orderController.orders[orderIndex];
+              final tasks = order['tasks'] as List<Map<String, dynamic>>;
+              final completedTasks =
+                  tasks.where((task) => task['completed'].value).length;
+              final progressPercentage = (completedTasks / tasks.length) * 100;
+
+              return ListTile(
+                leading: CircularProgressIndicator(
+                  value: completedTasks / tasks.length,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                  backgroundColor: Colors.grey.shade200,
+                ),
+                title: Text('Order by: ${order['customerName']}'),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Order date: ${order['orderDate']}'),
+                    Text('Progress: ${progressPercentage.toStringAsFixed(2)}%'),
+                  ],
+                ),
+              );
+            },
+          );
+        }),
       ),
-      body: Obx(() {
-        if (orderController.orders.isEmpty) {
-          return Center(child: CircularProgressIndicator());
-        }
-
-        return ListView.builder(
-          itemCount: orderController.orders.length,
-          itemBuilder: (context, orderIndex) {
-            final order = orderController.orders[orderIndex];
-            final tasks = order['tasks'] as List<Map<String, dynamic>>;
-            final completedTasks = tasks.where((task) => task['completed'].value).length;
-            final progressPercentage = (completedTasks / tasks.length) * 100;
-
-            return ListTile(
-              leading: CircularProgressIndicator(
-                value: completedTasks / tasks.length,
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
-                backgroundColor: Colors.grey.shade200,
-              ),
-              title: Text('Order by: ${order['customerName']}'),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Order date: ${order['orderDate']}'),
-                  Text('Progress: ${progressPercentage.toStringAsFixed(2)}%'),
-                ],
-              ),
-            );
-          },
-        );
-      }),
     );
   }
 }
--- lib/pages/task_page.dart
+++ lib/pages/task_page.dart
@@ -2,70 +2,130 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:new_trial2/controllers/order_controller.dart';
 
+import '../utils/color_utils.dart';
+
+
 class TaskPage extends StatelessWidget {
   final OrderController orderController = Get.put(OrderController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Task Management'),
-      ),
-      body: FutureBuilder(
-        future: orderController.fetchOrders(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else {
-            return Obx(() {
-              return ListView.builder(
-                itemCount: orderController.orders.length,
-                itemBuilder: (context, orderIndex) {
-                  final order = orderController.orders[orderIndex];
-                  return Card(
-                    margin: EdgeInsets.all(10),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Customer: ${order['customerName']}'),
-                          Text('Order Date: ${order['orderDate']}'),
-                          SizedBox(height: 10),
-                          // Display tasks for this order
-                          ListView.builder(
-                            shrinkWrap: true,
-                            physics:
-                                NeverScrollableScrollPhysics(), // Prevent scroll conflict
-                            itemCount: order['tasks'].length,
-                            itemBuilder: (context, taskIndex) {
-                              final task = order['tasks'][taskIndex];
-                              return Obx(() {
-                                return CheckboxListTile(
-                                  title: Text(task['name']),
-                                  value: task['completed']
-                                      .value, // Reactively observe the value
-                                  onChanged: (bool? value) {
-                                    if (value != null) {
-                                      orderController.toggleTaskCompletion(
-                                          orderIndex, taskIndex);
-                                    }
-                                  },
-                                );
-                              });
-                            },
-                          ),
-                        ],
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              hexStringToColor("E7C6A5"),
+              hexStringToColor("F4DBD8"),
+              hexStringToColor("F4DBD8"),
+              hexStringToColor("E7C6A5"),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: FutureBuilder(
+          future: orderController.fetchOrders(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              return Obx(() {
+                return ListView.builder(
+                  itemCount: orderController.orders.length,
+                  itemBuilder: (context, orderIndex) {
+                    final order = orderController.orders[orderIndex];
+                    return Card(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                    ),
-                  );
-                },
-              );
-            });
-          }
-        },
+                      elevation: 4,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Customer: ${order['customerName']}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              'Order Date: ${order['orderDate']}',
+                              style: TextStyle(
+                                color: Colors.grey.shade600,
+                                fontSize: 14,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Tasks:',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                                color: Colors.black87,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            // Display tasks for this order
+                            ListView.builder(
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              itemCount: order['tasks'].length,
+                              itemBuilder: (context, taskIndex) {
+                                final task = order['tasks'][taskIndex];
+                                return Obx(() {
+                                  return Container(
+                                    margin: EdgeInsets.symmetric(vertical: 5),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(8),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.shade300,
+                                          blurRadius: 5,
+                                          offset: Offset(0, 2),
+                                        ),
+                                      ],
+                                    ),
+                                    child: CheckboxListTile(
+                                      title: Text(
+                                        task['name'],
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                      value: task['completed'].value,
+                                      activeColor: Colors.green,
+                                      onChanged: (bool? value) {
+                                        if (value != null) {
+                                          orderController.toggleTaskCompletion(
+                                              orderIndex, taskIndex);
+                                        }
+                                      },
+                                    ),
+                                  );
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              });
+            }
+          },
+        ),
       ),
     );
   }
